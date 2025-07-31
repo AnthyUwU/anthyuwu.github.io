ecba1973fedcacb82bcecfa22c51c2b7b17f1565
Anthy <56406604+AnthyUwU@users.noreply.github.com>
2025-07-31 15:51:46 -0300

    Update page.tsx
    discord link

--- src/app/manual-aplicacao/page.tsx
+++ src/app/manual-aplicacao/page.tsx
@@ -14,7 +14,7 @@ export default function Home(){
 			Se você tiver alguma dúvida com o vídeo ou precisar de mais ajuda, sinta-se livre para nos chamar no Discord ou entrar no nosso servidor para tirar suas dúvidas!
 			</p>
 
-			<a href="https://discord.gg/dm49CtfVju">https://discord.gg/dm49CtfVju</a>
+			<a href="https://discord.gg/tvSDezQGQU">https://discord.gg/tvSDezQGQU</a>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/wsofV8BSmRU" title="Perlutan tutorial" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 		  </main>
 		</div>
